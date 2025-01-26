dcdf516f687bccf284eab2678981cb2d25491179
LucianCrainic <crainic.lucian@gmail.com>
2025-01-27 00:27:07 +0100

    [UPDATE] plots functions handle dataset name when saving the figs

--- src/functions/plots.py
+++ src/functions/plots.py
@@ -6,7 +6,9 @@ import numpy as np
 from config.config import IMAGES_PATH
 
 
-def plot_losses(train_losses: List[float], val_losses: List[float]) -> None:
+def plot_losses(
+    train_losses: List[float], val_losses: List[float], dataset_name: str
+) -> None:
     plt.figure(figsize=(12, 5))
     plt.subplot(1, 2, 1)
     plt.plot(train_losses, label="Train Loss")
@@ -14,10 +16,12 @@ def plot_losses(train_losses: List[float], val_losses: List[float]) -> None:
     plt.xlabel("Epoch")
     plt.ylabel("Loss")
     plt.legend()
-    plt.savefig(IMAGES_PATH + "loss_plot.png")
+    plt.savefig(IMAGES_PATH + "train/" + dataset_name + "/loss.png")
 
 
-def plot_accuracies(train_accs: List[float], val_accs: List[float]) -> None:
+def plot_accuracies(
+    train_accs: List[float], val_accs: List[float], dataset_name: str
+) -> None:
     plt.figure(figsize=(12, 5))
     plt.subplot(1, 2, 2)
     plt.plot(train_accs, label="Train Acc")
@@ -25,10 +29,10 @@ def plot_accuracies(train_accs: List[float], val_accs: List[float]) -> None:
     plt.xlabel("Epoch")
     plt.ylabel("Accuracy")
     plt.legend()
-    plt.savefig(IMAGES_PATH + "acc_plot.png")
+    plt.savefig(IMAGES_PATH + "train/" + dataset_name + "/accuracy.png")
 
 
-def plot_confusion_matrix(conf_matrix: np.ndarray) -> None:
+def plot_confusion_matrix(conf_matrix: np.ndarray, dataset_name: str) -> None:
     plt.figure(figsize=(8, 6))
     plt.imshow(conf_matrix, interpolation="nearest", cmap=plt.cm.Blues)
     plt.title("Confusion Matrix")
@@ -36,5 +40,4 @@ def plot_confusion_matrix(conf_matrix: np.ndarray) -> None:
     plt.xlabel("Predicted Labels")
     plt.ylabel("True Labels")
     plt.grid(False)
-    plt.savefig("confusion_matrix.png")
-    plt.show()
+    plt.savefig(IMAGES_PATH + "test/" + dataset_name + "/confusion_matrix.png")
