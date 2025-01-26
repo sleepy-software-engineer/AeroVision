72920336a2c78e3d39d51b4fe9fa0510906be5f2
LucianCrainic <crainic.lucian@gmail.com>
2025-01-26 18:13:58 +0100

    [WIP] add CIFAR 100

--- src/main.py
+++ src/main.py
@@ -3,17 +3,20 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
-from model import ViT
+from functions.train import train
+from logger.LoggerFactory import LoggerFactory
+from model import TinyViT
 
-if __name__ == "__main__":
+logger = LoggerFactory.get_logger(__name__)
+
+
+def cifar_10():
     train_transform = transforms.Compose(
         [
             transforms.RandomCrop(32, padding=4),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
-            transforms.Normalize(
-                (0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)
-            ),  # CIFAR-10 stats
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)),
         ]
     )
 
@@ -34,57 +37,93 @@ if __name__ == "__main__":
     train_loader = DataLoader(train_set, batch_size=128, shuffle=True)
     test_loader = DataLoader(test_set, batch_size=128, shuffle=False)
 
-    print("Train set size:", len(train_set))
-    print("Test set size:", len(test_set))
-
-    # Model, Loss, Optimizer
-    model = ViT(num_classes=10, embed_dim=64, num_heads=4, num_layers=6)
+    model = TinyViT(
+        num_classes=10,
+        embed_dim=64,
+        img_size=32,
+        patch_size=4,
+        in_chans=3,
+        num_heads=4,
+        num_layers=6,
+        mlp_dim=256,
+    )
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4, weight_decay=0.05)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
 
-    num_epochs = 50
-    for epoch in range(num_epochs):
-        # Training Phase
-        model.train()
-        train_loss = 0.0
-        train_correct = 0
-        for images, labels in train_loader:
-            images, labels = images.to(device), labels.to(device)
+    train(
+        model,
+        train_loader,
+        test_loader,
+        criterion,
+        optimizer,
+        device,
+        num_epochs=200,
+        patience=25,
+        train_set_size=len(train_set),
+        val_set_size=len(test_set),
+    )
 
-            optimizer.zero_grad()
-            outputs = model(images)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
 
-            train_loss += loss.item()
-            _, preds = torch.max(outputs, 1)
-            train_correct += (preds == labels).sum().item()
+def cifar_100():
+    train_transform = transforms.Compose(
+        [
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)),
+        ]
+    )
 
-        # Validation Phase
-        model.eval()
-        val_loss = 0.0
-        val_correct = 0
-        with torch.no_grad():
-            for images, labels in test_loader:
-                images, labels = images.to(device), labels.to(device)
-                outputs = model(images)
-                loss = criterion(outputs, labels)
+    test_transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)),
+        ]
+    )
 
-                val_loss += loss.item()
-                _, preds = torch.max(outputs, 1)
-                val_correct += (preds == labels).sum().item()
+    train_set = datasets.CIFAR100(
+        root="./data", train=True, download=True, transform=train_transform
+    )
+    test_set = datasets.CIFAR100(
+        root="./data", train=False, download=True, transform=test_transform
+    )
 
-        # Compute Metrics
-        train_loss = train_loss / len(train_loader)
-        train_acc = train_correct / len(train_set)
-        val_loss = val_loss / len(test_loader)
-        val_acc = val_correct / len(test_set)
+    train_loader = DataLoader(train_set, batch_size=128, shuffle=True)
+    test_loader = DataLoader(test_set, batch_size=128, shuffle=False)
 
-        print(f"Epoch {epoch + 1}/{num_epochs}")
-        print(f"Train Loss: {train_loss:.4f} | Train Acc: {train_acc * 100:.2f}%")
-        print(f"Val Loss: {val_loss:.4f} | Val Acc: {val_acc * 100:.2f}%")
-        print("--------------------------")
+    model = TinyViT(
+        num_classes=100,
+        embed_dim=64,
+        img_size=32,
+        patch_size=4,
+        in_chans=3,
+        num_heads=4,
+        num_layers=6,
+        mlp_dim=256,
+    )
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4, weight_decay=0.05)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+
+    train(
+        model,
+        train_loader,
+        test_loader,
+        criterion,
+        optimizer,
+        device,
+        num_epochs=200,
+        patience=25,
+        train_set_size=len(train_set),
+        val_set_size=len(test_set),
+    )
+
+
+if __name__ == "__main__":
+    cifar_100()
